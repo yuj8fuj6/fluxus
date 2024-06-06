b1b9c6909cf34a885b0d98d7857d7fd384732e4e
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2024-06-06 17:22:20 +0800

    Added details to Checker Page

--- src/components/ModelChecker.tsx
+++ src/components/ModelChecker.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useActionContext } from "../contexts/ActionContext";
 import { useAuthActions } from "../hooks/useAuthActions";
 import { STREAM_ID, COMMIT_ID, OBJECT_ID } from "../speckleUtils";
+import { Building, Building2 } from "lucide-react";
 import { ObjectLayers } from "@speckle/viewer";
 
 const ModelChecker = () => {
@@ -25,12 +26,50 @@ const ModelChecker = () => {
     initializeObject();
   }, []);
 
-  console.log(currentCommit?.id);
+  console.log(currentCommit?.createdAt);
   console.log(model);
 
   return (
-    <div className="absolute mt-8 ml-32 w-[20rem] h-3/4 z-10 bg-white drop-shadow-lg rounded-lg grid grid-cols-1 content-start gap-y-4 p-4 text-[#C71585]">
-      <div className="flex justify-start font-bold text-xl">IFC-SG Checker</div>
+    <div className="absolute mt-8 ml-32 w-[20rem] h-3/4 z-10 bg-white drop-shadow-lg rounded-lg grid grid-cols-1 content-start gap-y-4 p-4">
+      <div className="flex justify-start font-bold text-xl text-[#C71585]">
+        IFC-SG Checker
+      </div>
+      {model ? (
+        <div className="grid grid-cols-2 gap-y-2 text-sm">
+          <div className="col-span-1 text-[#C71585] flex justify-start font-semibold">
+            Project:
+          </div>
+          <div className="col-span-1 flex justify-end">{model.name}</div>
+          <div className="col-span-1 text-[#C71585] flex justify-start font-semibold">
+            Commit Branch:
+          </div>
+          <div className="col-span-1 flex justify-end">
+            {currentCommit?.branchName}
+          </div>
+          <div className="col-span-1 text-[#C71585] flex justify-start font-semibold">
+            Model Type:
+          </div>
+          <div className="col-span-1 flex justify-end">
+            {currentCommit?.sourceApplication}
+          </div>
+          <div className="col-span-1 text-[#C71585] flex justify-start font-semibold">
+            Created At:
+          </div>
+          <div className="col-span-1 flex justify-end">
+            {currentCommit?.createdAt
+              ? new Date(currentCommit?.createdAt).toLocaleDateString("en-US")
+              : "Invalid date"}
+          </div>
+        </div>
+      ) : (
+        <div className="text-gray-200 flex flex-col items-center gap-y-8 mt-40">
+          <Building2 size={150} strokeWidth={1} />
+          <div className="italic font-semibold">
+            No model loaded.
+            <br /> Load a model for validation.
+          </div>
+        </div>
+      )}
     </div>
   );
 };
