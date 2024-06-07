9b435ab97ea03783b9b853a8192afef144428b7e
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2024-06-07 15:27:21 +0800

    Implemented base dropdown menu for validated items

--- src/components/ModelChecker.tsx
+++ src/components/ModelChecker.tsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useActionContext } from "../contexts/ActionContext";
 import { useAuthActions } from "../hooks/useAuthActions";
 import { STREAM_ID, COMMIT_ID, OBJECT_ID } from "../speckleUtils";
-import { Building2 } from "lucide-react";
+import {
+  Building2,
+  Filter,
+  ChevronDown,
+  RotateCcw,
+  ChevronUp,
+} from "lucide-react";
 import { ObjectLayers } from "@speckle/viewer";
 
 import { Separator } from "./ui/separator";
@@ -14,13 +20,19 @@ const ModelChecker = () => {
   const { state } = useActionContext();
   const { currentCommit } = state;
   const { handleCommitSelection, fetchObject } = useAuthActions();
-  const commitId = localStorage.getItem(COMMIT_ID);
-  const streamId = localStorage.getItem(STREAM_ID);
-  const objectId = localStorage.getItem(OBJECT_ID);
-
   const [model, setModel] = useState<any>();
   const [file, setFile] = useState<File | null>(null);
   const { toast } = useToast();
+  const [checkModel, setCheckModel] = useState<boolean>(true);
+  const [showCompliantDropdown, setShowCompliantDropdown] =
+    useState<boolean>(false);
+  const [showNonCompliantDropdown, setShowNonCompliantDropdown] =
+    useState<boolean>(false);
+  const [showIssueDropdown, setShowIssueDropdown] = useState<boolean>(false);
+
+  const commitId = localStorage.getItem(COMMIT_ID);
+  const streamId = localStorage.getItem(STREAM_ID);
+  const objectId = localStorage.getItem(OBJECT_ID);
 
   useEffect(() => {
     const initializeObject = async () => {
@@ -34,7 +46,7 @@ const ModelChecker = () => {
   }, []);
 
   return (
-    <div className="absolute mt-8 ml-32 w-[20rem] h-3/4 z-10 bg-white drop-shadow-lg rounded-lg grid grid-cols-1 content-start gap-y-4 p-4">
+    <div className="absolute mt-8 ml-32 w-[20rem] max-h-[800px] z-10 bg-white drop-shadow-lg rounded-lg grid grid-cols-1 content-start gap-y-4 p-4">
       <div className="flex justify-start font-bold text-xl text-[#C71585]">
         IFC-SG Checker
       </div>
@@ -91,6 +103,7 @@ const ModelChecker = () => {
                   title: "Success!",
                   description: "Speckle commit loaded successfully.",
                 });
+                setCheckModel(true);
               } catch (error) {
                 toast({
                   variant: "destructive",
@@ -105,6 +118,123 @@ const ModelChecker = () => {
             Check Model
           </Button>
           <Separator className="col-span-6" />
+          {checkModel ? (
+            <div className="col-span-6 space-y-3">
+              <div className="col-span-3 text-[#C71585] flex justify-start font-semibold">
+                Validator Results:
+              </div>
+              <div className="col-span-6 grid grid-cols-3 gap-x-3 text-[#C71585] ">
+                <Button
+                  size="sm"
+                  variant="outlined"
+                  className="text-xs"
+                  onClick={() => {}}
+                >
+                  Filter <Filter size={15} className="ml-3" />
+                </Button>
+                <Button
+                  size="sm"
+                  variant="outlined"
+                  className="text-xs"
+                  onClick={() => {}}
+                >
+                  Sort <ChevronDown size={15} className="ml-3" />
+                </Button>
+                <Button
+                  size="sm"
+                  variant="outlined"
+                  className="text-xs"
+                  onClick={() => {}}
+                >
+                  Reset <RotateCcw size={15} className="ml-3" />
+                </Button>
+              </div>
+              <button
+                className="w-full inline-flex items-center justify-between whitespace-nowrap rounded-xl text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-2 border-red-500 bg-background hover:bg-red-500 hover:text-white p-2 text-red-500"
+                disabled={showCompliantDropdown || showIssueDropdown}
+                onClick={() =>
+                  setShowNonCompliantDropdown(!showNonCompliantDropdown)
+                }
+              >
+                Non-Compliant{" "}
+                <div className="flex">
+                  5000 items
+                  {showNonCompliantDropdown ? (
+                    <ChevronUp className="ml-2" />
+                  ) : (
+                    <ChevronDown className="ml-2" />
+                  )}
+                </div>
+              </button>
+              {showNonCompliantDropdown && (
+                <div className="absolute z-10 w-72 bg-white shadow-lg max-h-72 overflow-auto mt-1 animate-slideDown rounded-xl">
+                  <ul>
+                    {Array.from({ length: 10 }).map((_, index) => (
+                      <li key={index} className="p-2 hover:bg-gray-100">
+                        Item {index + 1}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              <button
+                className="w-full inline-flex items-center justify-between whitespace-nowrap rounded-xl text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-2 border-[#FFA800] bg-background hover:bg-[#FFA800] hover:text-white p-2 text-[#FFA800]"
+                disabled={showCompliantDropdown || showNonCompliantDropdown}
+                onClick={() => setShowIssueDropdown(!showIssueDropdown)}
+              >
+                Possible Issue{" "}
+                <div className="flex">
+                  5000 items{" "}
+                  {showIssueDropdown ? (
+                    <ChevronUp className="ml-2" />
+                  ) : (
+                    <ChevronDown className="ml-2" />
+                  )}
+                </div>
+              </button>
+              {showIssueDropdown && (
+                <div className="absolute z-10 w-72 bg-white shadow-lg max-h-72 overflow-auto mt-1 animate-slideDown rounded-xl">
+                  <ul>
+                    {Array.from({ length: 10 }).map((_, index) => (
+                      <li key={index} className="p-2 hover:bg-gray-100">
+                        Item {index + 1}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              <button
+                className="w-full inline-flex items-center justify-between whitespace-nowrap rounded-xl text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border-2 border-green-400 bg-background hover:bg-green-400 hover:text-white p-2 text-green-400"
+                disabled={showNonCompliantDropdown || showIssueDropdown}
+                onClick={() => setShowCompliantDropdown(!showCompliantDropdown)}
+              >
+                Compliant{" "}
+                <div className="flex">
+                  5000 items{" "}
+                  {showCompliantDropdown ? (
+                    <ChevronUp className="ml-2" />
+                  ) : (
+                    <ChevronDown className="ml-2" />
+                  )}
+                </div>
+              </button>
+              {showCompliantDropdown && (
+                <div className="absolute z-10 w-72 bg-white shadow-lg max-h-72 overflow-auto mt-1 animate-slideDown rounded-xl">
+                  <ul>
+                    {Array.from({ length: 10 }).map((_, index) => (
+                      <li key={index} className="p-2 hover:bg-gray-100">
+                        Item {index + 1}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="text-gray-400 col-span-6 flex justify-center items-center italic">
+              Upload IFC-SG mapping file and check model for validation.
+            </div>
+          )}
         </div>
       ) : (
         <div className="text-gray-200 flex flex-col items-center gap-y-8 mt-40">
--- tailwind.config.js
+++ tailwind.config.js
@@ -70,11 +70,16 @@ module.exports = {
           "0%, 100%": { "background-position": "left center" },
           "50%": { "background-position": "right center" },
         },
+        slideDown: {
+          "0%": { transform: "translateY(-10px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         gradient: "gradient 10s ease infinite",
+        slideDown: "slideDown 0.3s ease-out forwards",
       },
     },
   },
